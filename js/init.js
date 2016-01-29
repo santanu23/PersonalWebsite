612f7557071e1ddf2dc0e401f39055633808d900
santanu23 <sarkersantanu@gmail.com>
2016-01-28 22:45:50 -0500

    navbar
    fixed navbar to the top

--- js/init.js
+++ js/init.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-     $('.parallax').parallax();
-     $('.slider').slider({full_width: true, indicators: true, height: 620, interval:10000});
-   });
-$(document).ready(function(){
-   $('a[href^="#"]').on('click',function (e) {
-       e.preventDefault();
-       var target = this.hash;
-       $target = $(target);
-       $('html, body').stop().animate({
-           'scrollTop':  $target.offset().top //no need of parseInt here
-       }, 900, 'swing', function () {
-           window.location.hash = target;
-       });
-   });
+  $('.parallax').parallax();
+  $('.slider').slider({full_width: true, indicators: true, height: 620, interval:10000});
+  $(".button-collapse").sideNav();
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+    var target = this.hash;
+    $target = $(target);
+    $('html, body').stop().animate({
+       'scrollTop':  $target.offset().top - $('nav').height()//no need of parseInt here
+
+    }, 900, 'swing', function () {
+       window.location.hash = target;
+    });
+  });
 });
